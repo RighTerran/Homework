a8d778b480a53ef8b24526e5a9bc7207f2931d9a
Dmitriy <sctoss@gmail.com>
2022-08-04 20:40:43 +0300

    Completed task25

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задаем с клавиатуры
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите 8 элементов массива: ");
+int[] myArray = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
