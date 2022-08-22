b0f46d5a30fbdf35d671d23bd28855f41fd88f9b
Dmitriy <sctoss@gmail.com>
2022-08-22 20:28:39 +0300

    Completed task60

---
+++ task60/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+массив размером 2 x 2 x 2
+12(0,0,0) 22(0,0,1)
+45(1,0,0) 53(1,0,1)
+*/
+
+int[,,] myArray = new int[2, 2, 2];
+
+Random random = new Random();
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        for (int k = 0; k < myArray.GetLength(2); k++)
+        {
+            myArray[i, j, k] = random.Next(10,100);
+        }
+    }
+}
+
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        for (int k = 0; k < myArray.GetLength(2); k++)
+        {
+            Console.Write($"{myArray[i, j, k]} ({i},{j},{k})  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
