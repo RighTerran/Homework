89ba21c812db31d10cf190d2a448e98a64f97184
Dmitriy <sctoss@gmail.com>
2022-08-22 20:28:22 +0300

    Completed task58

---
+++ task58/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, заданы 2 массива:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+и
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+
+Их произведение будет равно следующему массиву:
+
+1 20 56 10
+20 81 8 6
+56 8 4 24
+10 6 24 49
+*/
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiplicationOfArrays(int[,] array1, int[,] array2, int[,] result)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                sum += array1[i, k] * array2[k, j];
+            }
+            result[i, j] = sum;
+        }
+    }
+    return result;
+}
+
+int[,] myArray1 =
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+    {5, 2, 6, 7}
+};
+
+int[,] myArray2 =
+{
+    {1, 5, 8, 5},
+    {4, 9, 4, 2},
+    {7, 2, 2, 6},
+    {2, 3, 4, 7}
+};
+
+Console.Write("Массив №1:\n");
+PrintArray(myArray1);
+
+Console.Write("\nМассив №2:\n");
+PrintArray(myArray2);
+
+int[,] result = new int[myArray1.GetLength(0), myArray2.GetLength(1)];
+
+Console.Write("\nРезультат умножения матриц:\n"); // в примере в задании указано поэлементное умножение
+MultiplicationOfArrays(myArray1, myArray2, result);
+PrintArray(result);
